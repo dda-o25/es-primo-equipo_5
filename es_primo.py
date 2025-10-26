ca07763d5996b2b5b3a1a9ac666b187ca9b56e70
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2025-10-26 03:08:43 -0600

    Capture team solution

--- es_primo.py
+++ es_primo.py
@@ -1,15 +1,17 @@
-"""
-Inserta el encabezado aquí y escribe tu código abajo
-"""
-
-# Declaraciones
-CONSTANTE = valor
-
-# Entradas
-entrada = input()
-
-# Proceso
-
-
-# Salidas
-print(salida)
+#Determinar si un número dado es primo
+#Eduardo Caleb Castillo Llanas
+#Daniel Maldonado Delgado
+#Larisa Carolina Alvarez Gonzales
+#Ximena Castro Flores
+#10/Octubre/25
+#entrdas
+numero = int(input("Dame un numero para ver si es primo: "))
+primo = False
+#desarrollo
+for n in range(2,numero):
+  if numero % n == 0:
+    print("el numero",numero, "no es primo")#Salida
+    break
+else:
+  primo =True
+  print("el numero",numero," es primo")#Salida
